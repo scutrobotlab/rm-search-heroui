d91b5889fec4dc32d8e67a1e1d211b91e0f0c65a
Summer-lights <1067088037@qq.com>
2025-02-12 23:19:49 +0800

    feat: update site title and description to reflect RM Search branding

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Vite + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "RM Search",
+  description: "The Best Search Engine for RoboMaster",
   navItems: [
     {
       label: "Home",
