c14278f45c9a964997285c727a877b55d2d5d5eb
Summer-lights <1067088037@qq.com>
2025-07-19 13:14:37 +0800

    feat: add click handler to word cloud for search functionality

--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -42,17 +42,22 @@ export default function StatisticsPage() {
     fetchData();
   }, []);
 
+  const onChartClick = (e: any) => {
+    const word = e?.datum?.word;
+
+    window.location.href = `/search/?query=${word}`;
+  };
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 mt-20">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>统计</h1>
         </div>
-      </section>
 
-      <div>
-        <VChart spec={spec} />
-      </div>
+        <b className={"text-2xl mt-4"}>搜索词云</b>
+        <VChart spec={spec} onClick={onChartClick} />
+      </section>
     </DefaultLayout>
   );
 }
